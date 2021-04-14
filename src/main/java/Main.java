b2238cf0b42b9082230cdb2c64ce743c8b8a9f36
Nacho <i.vallejos01@ufromail.cl>
2021-04-14 12:42:31 -0400

    /nacho

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 public class Main {
+<<<<<<< 06d93e2338a54a889c2131dd73530485b3b1f4ac
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!");
         System.out.println("Vicente Silva Berrocal");
 
+=======
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!!");
+>>>>>>> /nacho
     }
 }
