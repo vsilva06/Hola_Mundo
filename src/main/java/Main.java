045b4d84bd76c85b052cb36f882d8f0068530d38
Vicente <v.silva06@ufromail.cl>
2021-04-14 13:09:12 -0400

    Revert "Agregado nombre"
    This reverts commit b9da9220ed891e6965cf54d025a108a473d0551c.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!");
-<<<<<<< 06d93e2338a54a889c2131dd73530485b3b1f4ac
         System.out.println("Vicente Silva Berrocal");
 
 =======
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!");
 >>>>>>> /nacho
+<<<<<<< HEAD
 =======
         System.out.println("Ignacio Vallejos Ebner");
 >>>>>>> Agregado nombre
@@ -19,5 +19,7 @@ public class Main {
         System.out.println("Hola Mundo!!!");
         System.out.println("Ignacio Vallejos Ebner");
 >>>>>>> Nacho
+=======
+>>>>>>> parent of b9da922... Agregado nombre
     }
 }
