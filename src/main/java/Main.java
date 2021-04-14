b9da9220ed891e6965cf54d025a108a473d0551c
Nacho <i.vallejos01@ufromail.cl>
2021-04-14 12:46:01 -0400

    Agregado nombre

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!");
+<<<<<<< 06d93e2338a54a889c2131dd73530485b3b1f4ac
         System.out.println("Vicente Silva Berrocal");
 
 =======
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!");
 >>>>>>> /nacho
+=======
+        System.out.println("Ignacio Vallejos Ebner");
+>>>>>>> Agregado nombre
     }
 }
